24d7c0a5b20466f6e6dbacca57ac847e25c930f5
Vicky <vickygupta9987@gmail.com>
2024-09-21 14:39:56 +0530

    commit

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,1 +1,3 @@
-export const SERVER_BASE_URL = "http://localhost:5000";
+// export const SERVER_BASE_URL = "http://localhost:5000";
+export const SERVER_BASE_URL =
+  "https://product-inventory-management.onrender.com";
